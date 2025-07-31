172f1bd1efa9b5556207e5e48a02cabf0fd9d7e2
YannickNQ <yninaq@outlook.es>
2025-07-31 15:28:07 -0500

    REFs #1 feat: initialize CoinFluxCraft project

---
+++ src/main/alura/com/coinfluxcraft/Main.java
@@ -0,0 +1,19 @@
+package main.alura.com.coinfluxcraft;
+
+/**
+ * CoinFluxCraft - Currency Converter Application
+ * A simple and efficient currency conversion tool
+ *
+ * @author Yannick.Dev
+ * @version 1.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to CoinFluxCraft!");
+        System.out.println("Currency Converter v1.0");
+
+        // TODO: Initialize application
+    }
+}
