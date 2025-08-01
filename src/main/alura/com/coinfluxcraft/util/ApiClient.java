e8520c55db2921ea579a80e7a18c92dcb1c51042
YannickNQ <yninaq@outlook.es>
2025-07-31 21:02:55 -0500

    REFs #5, #6 feat: add HttpRequest and HttpResponse to handle Exchange Rate API

--- src/main/alura/com/coinfluxcraft/util/ApiClient.java
+++ src/main/alura/com/coinfluxcraft/util/ApiClient.java
@@ -1,11 +1,33 @@
 package main.alura.com.coinfluxcraft.util;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class ApiClient {
     private static final HttpClient httpClient = HttpClient.newBuilder()
             .connectTimeout(Duration.ofSeconds(10))
             .build();
+
+    public static String sendGetRequest(String url) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .timeout(Duration.ofSeconds(30))
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return response.body();
+        } else {
+            throw  new RuntimeException("HTTP Error: " + response.statusCode());
+        }
+    }
 
 }
